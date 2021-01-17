717331b1fe895ac80d1e03f4c7e3e1c5d99507ae
sk.roh <seongkwan.roh@gmail.com>
2021-01-18 01:04:01 +0900

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with firebase
+          Learn React with firebase automatic deploy.
         </a>
       </header>
     </div>
